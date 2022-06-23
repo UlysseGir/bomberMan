fac0a0d88522bdd621d6fee358c2b81fdb3f920f
lecanardjaune <ugiraud@edenschool.fr>
2022-06-23 13:40:05 +0200

    keydown check

--- js/app.js
+++ js/app.js
@@ -1,6 +1,31 @@
 const app = document.getElementById("app");
-
+const player = document.getElementById("player");
 
 for(let i = 0; i < 200; i++){
-    app.innerHTML += `<img src="img/ground.png" alt="">`
+    app.innerHTML += `<img src="img/ground.png" alt="">`;
 }
+
+
+
+document.addEventListener('keydown', (event) => {
+    const key = event.key;
+
+    if(key === "z"){
+        console.log("up");
+        player.style.backgroundColor = "red"
+    }
+
+    if(key === "s"){
+        console.log("down");
+    }
+
+    if(key === "q"){
+        console.log("left");
+    }
+
+    if(key === "d"){
+        console.log("right");
+    }
+
+  });
+  
