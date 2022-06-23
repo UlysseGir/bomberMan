99724d1f384e0bb515035ccdab43d43bc80b2e04
lecanardjaune <ugiraud@edenschool.fr>
2022-06-23 15:21:49 +0200

    first character move

--- js/app.js
+++ js/app.js
@@ -1,31 +1,57 @@
 const app = document.getElementById("app");
-const player = document.getElementById("player");
 
 for(let i = 0; i < 200; i++){
     app.innerHTML += `<img src="img/ground.png" alt="">`;
 }
 
 
-
+let right = 0;
+let tb = 0;
+let direction = "";
 document.addEventListener('keydown', (event) => {
     const key = event.key;
+    const player = document.getElementById("player");
 
-    if(key === "z"){
-        console.log("up");
-        player.style.backgroundColor = "red"
-    }
-
-    if(key === "s"){
-        console.log("down");
-    }
-
-    if(key === "q"){
-        console.log("left");
-    }
-
-    if(key === "d"){
-        console.log("right");
+    switch(event.key){
+        case "z":
+            direction = "up"
+            console.log(direction)
+            break;
+        case "d":
+            direction = "right"
+            break;
+        case "q":
+            direction = "left"
+            break;
+        case "s":
+            direction = "down"
+            break;
     }
 
   });
   
+  document.addEventListener('keyup', (event) => {
+    direction = null;
+    console.log(direction)
+  })
+
+const time = setInterval(()=>{
+    switch(direction){
+        case "up":
+            tb -= 10;
+            player.style.top = tb + "px";
+            break;
+        case "down":
+            tb += 10;
+            player.style.top = tb + "px";
+            break;
+        case "left":
+            right += 10;
+            player.style.right = right + "px";
+            break;
+        case "right":
+            right -= 10;
+            player.style.right = right + "px";
+            break;         
+    }
+}, 20)
